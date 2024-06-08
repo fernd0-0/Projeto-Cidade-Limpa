8e59ca6857df33a7fb5460a15c5e787ce84c54d0
fernd0-0 <115960017+fernd0-0@users.noreply.github.com>
2024-06-08 10:38:32 -0300

    Delete src directory

--- src/models/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/projeto-cidade-limpa').then(() => {
-  console.log('Conectado ao MongoDB');
-}).catch((err) => {
-  console.log('Erro ao conectar ao MongoDB:', err);
-});
